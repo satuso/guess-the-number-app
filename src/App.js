57fb49c4a1023dfb82f651c4779c2d2c6e00c1a9
satuso <73895314+satuso@users.noreply.github.com>
2022-08-28 22:04:32 +0300

    clean up code and update README.md

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import {useState, useEffect} from "react"
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  const [guess, setGuess] = useState("")
+  const [guess, setGuess] = useState('')
   const [guessList, setGuessList] = useState([])
   const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random () * 100) + 1)
   const [counter, setCounter] = useState(10)
   const [counterMessage, setCounterMessage] = useState(`Guesses left: ${counter}`)
-  const [message, setMessage] = useState("")
-  const [msgStyle, setMsgStyle] = useState({color:"black"})
-  const [btnStyle, setBtnStyle] = useState({backgroundColor:"lightgray"})
+  const [message, setMessage] = useState('')
+  const [msgStyle, setMsgStyle] = useState({ color:'black' })
+  const [btnStyle, setBtnStyle] = useState({ backgroundColor:'lightgray' })
   const [disabled, setDisabled] = useState(false)
 
   useEffect(
@@ -20,60 +20,59 @@ function App() {
   const handleInput = (event) => {
     event.preventDefault()
     setGuess(event.target.value)
-    setMsgStyle({color:"black"})
-    setMessage("")
+    setMsgStyle({ color:'black' })
+    setMessage('')
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    setBtnStyle({backgroundColor:"lightgray"})
+    setBtnStyle({ backgroundColor:'lightgray' })
     if (counter > 0 && guess >= 1 && guess <= 100){
       setCounter(counter => counter - 1)
-      setMessage("")
+      setMessage('')
       for (let i = 0; i < counter; i++){
         setGuessList(guessList.concat(guess))
-        setGuess("")
+        setGuess('')
         if (guess < randomNumber){
-          setMsgStyle({color:"black"})
+          setMsgStyle({ color:'black' })
           setCounterMessage(`Guesses left: ${counter}, number is too low`)
         }
         if (guess > randomNumber){
-          setMsgStyle({color:"black"})
+          setMsgStyle({ color:'black' })
           setCounterMessage(`Guesses left: ${counter}, number is too high`)
         }
-        // eslint-disable-next-line eqeqeq
         if (guess == randomNumber){
           setGuessList(guessList.concat(guess))
-          setMsgStyle({color:"tomato"})
-          setMessage("Congrats! You won")
-          setCounterMessage("")
+          setMsgStyle({ color:'tomato' })
+          setMessage('Congrats! You won')
+          setCounterMessage('')
           setDisabled(true)
-          setBtnStyle({backgroundColor:"coral"})
+          setBtnStyle({ backgroundColor:'coral' })
         }
       }
     } else if (counter === 0 && guess >= 1 && guess <= 100) {
       setGuessList(guessList.concat(guess))
-      setMsgStyle({color:"tomato"})
-      setMessage("Game Over! No win")
-      setCounterMessage("")
+      setMsgStyle({ color:'tomato' })
+      setMessage('Game Over! No win')
+      setCounterMessage('')
       setCounter(0)
       setDisabled(true)
-      setBtnStyle({backgroundColor:"coral"})
+      setBtnStyle({ backgroundColor:'coral' })
     } else {
-      setMsgStyle({color:"tomato"})
-      setMessage(`Guess must be a number between 1-100`)
-    }    
+      setMsgStyle({ color:'tomato' })
+      setMessage('Number must be between 1-100')
+    }
   }
 
   const handleReset = value => {
-    setGuess("")
+    setGuess('')
     setCounter(value-1)
     setGuessList([])
     setRandomNumber(Math.floor(Math.random () * (101 - 1) + 1))
-    setMsgStyle({color:"black"})
+    setMsgStyle({ color:'black' })
     setMessage(`Guesses left: ${value}`)
     setDisabled(false)
-    setBtnStyle({backgroundColor:"lightgray"})
+    setBtnStyle({ backgroundColor:'lightgray' })
   }
 
   return (
@@ -81,28 +80,28 @@ function App() {
       <h1>Guess the Number Game</h1>
       <p>Guess a random number between 1 and 100.</p>
       <p>Enter a number:</p>
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="number"
           value={guess}
           onChange={handleInput}
-          disabled = {disabled ? "disabled" : ""}
+          disabled = {disabled ? 'disabled' : ''}
         />
-        <button 
-          className="submit" 
+        <button
+          className="submit"
           type="submit"
-          disabled = {disabled ? "disabled" : ""}>Submit</button>
-        </form>
-      
-      <p>Previous guesses: {guessList.join(" ")}</p>
+          disabled = {disabled ? 'disabled' : ''}>Submit</button>
+      </form>
+
+      <p>Previous guesses: {guessList.join(' ')}</p>
       <p><b>{counterMessage}</b></p>
       <p style={msgStyle}><b>{message}</b></p>
-      <button 
-        onClick={() => 
+      <button
+        onClick={() =>
           handleReset(10)}
-          style={btnStyle}
-          disabled = {disabled ? "" : "disabled"}
-        >Play Again</button>
+        style={btnStyle}
+        disabled = {disabled ? '' : 'disabled'}
+      >Play Again</button>
     </div>
   )
 }
